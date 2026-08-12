c0cd3bffd3e9a0c760e2dc2ec75b52303090ac5c
Christeen <christeenhallak33@gmail.com>
2026-08-12 17:30:51 +0300

    finishing the  project architecture

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
 from routes.base import base_router
 
 app = FastAPI()
--- routes/base.py
+++ routes/base.py
@@ -1,10 +1,15 @@
 from fastapi import  APIRouter
+import os
 
 base_router = APIRouter()
 
 
 @base_router.get("/health")
 def health():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
     return {
-        "message":"Application is running successfully"
+        "message":"Application is running successfully",
+        "app_name":app_name,
+        "app_version":app_version
     }
